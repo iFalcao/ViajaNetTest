51ad59f313503e1e8c3ae56dc2719decb94bad6d
iFalcao <vbsfalcao@gmail.com>
2019-06-09 13:19:49 -0300

    Configure Static Files

---
+++ ViajaNet/Helpers/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViajaNet.Helpers
+{
+    public static class Extensions
+    {
+        public static void ConfigureStaticFilesWithDefault(this IApplicationBuilder app)
+        {
+            DefaultFilesOptions defaultFileOptions = new DefaultFilesOptions();
+            defaultFileOptions.DefaultFileNames.Clear();
+            defaultFileOptions.DefaultFileNames.Add("home.html");
+            app.UseDefaultFiles(defaultFileOptions);
+            app.UseStaticFiles();
+        }
+    }
+}
