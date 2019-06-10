b4e720c706a24aed44f0030dc77f1ff91476ae42
iFalcao <vbsfalcao@gmail.com>
2019-06-10 08:55:33 -0300

    Criação do projeto Domain

---
+++ Domain/DTOs/VisitDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViajaNet.Domain.DTOs
+{
+    public class VisitDTO
+    {
+        public string Url { get; set; }
+        public string Browser { get; set; }
+        public string Ip { get; set; }
+        public string PageParams { get; set; }
+    }
+}
---
+++ Domain/Models/Visit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViajaNet.Domain.Models
+{
+    [Serializable]
+    public class Visit
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Browser { get; set; }
+        public string Ip { get; set; }
+        public string PageParams { get; set; }
+    }
+}
